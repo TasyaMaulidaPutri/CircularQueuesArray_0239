f2c0a3b22722e9b807b32cf4ae1ac5bb9689c0c2
TasyaMaulidaPutri <tasyamaulidaputri06@gamil.com>
2025-06-10 11:12:59 +0700

    menambahkan loop untuk iterasi dari awal array hingga akhir REAR

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -86,9 +86,16 @@ public:
                 cout << queue_array[FRONT_position] << " ";
                 FRONT_position++;
             }
+
+            FRONT_position = 0;
+
+            // Iterasi dari awal array hingga REAR
+            while (FRONT_position <= REAR_position) {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout << endl;
         }
     }
-
-
 
 };
